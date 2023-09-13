516bd587b198bb2a5213dd133023393500aa6219
Yurii <yurii.askinderov@gmail.com>
2023-09-13 11:26:45 +0300

    Pushing HW2

---
+++ Hometask_2.py
@@ -0,0 +1,9 @@
+# task 2
+# print("a", "b", "c", sep="@")
+# print("x", "y", "z", end="!\n")
+# print("Tuesday", "Thursday", "Saturday", sep=" - 19:00, ", end=" - 11:00")
+
+
+# task 3
+print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#")
+
